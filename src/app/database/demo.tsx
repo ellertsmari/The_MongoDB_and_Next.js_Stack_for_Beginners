475f302f72a4b842e38d785e166f4c8ba965065d
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2024-01-10 16:16:34 +0000

    Update demo.tsx

--- src/app/database/demo.tsx
+++ src/app/database/demo.tsx
@@ -1,8 +1,5 @@
 import {MongoClient, ServerApiVersion} from 'mongodb'
 
-    
-const uri = "mongodb+srv://ellertsmari:smari2203@cluster0.lmfjbot.mongodb.net/?retryWrites=true&w=majority";
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
